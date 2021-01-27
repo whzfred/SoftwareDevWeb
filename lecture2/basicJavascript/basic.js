7ae8c120dbe76a8a71956dfc88f54ba2d3527f9d
Fredrik Stridh <fredrik.stridh@hkr.se>
2021-01-27 19:09:32 +0100

    basic.js example lecture 2

---
+++ lecture2/basicJavascript/basic.js
@@ -0,0 +1,17 @@
+/**
+ * This is some basics in JavaScript.
+ */
+
+// One line comment
+
+var someValue = 42;
+var someString = "42";
+
+if (someValue == "42") {
+    console.log("Yes, its 42 alright.");
+}
+
+if (someString === "42") {
+    console.log("Yes, its 42 alright.");
+}
+	
